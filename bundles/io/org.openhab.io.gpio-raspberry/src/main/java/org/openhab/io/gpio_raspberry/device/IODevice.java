1e3c30d9b848e190b750c8dd464ed499d54e88b0
Robert Delbrück <querdenker2k@gmx.de>
2016-05-14 12:04:21 +0200

    update pi4j lib

--- bundles/io/org.openhab.io.gpio-raspberry/src/main/java/org/openhab/io/gpio_raspberry/device/IODevice.java
+++ bundles/io/org.openhab.io.gpio-raspberry/src/main/java/org/openhab/io/gpio_raspberry/device/IODevice.java
@@ -16,7 +16,6 @@ import com.pi4j.io.gpio.GpioFactory;
 import com.pi4j.io.gpio.GpioPinDigitalInput;
 import com.pi4j.io.gpio.GpioPinDigitalOutput;
 import com.pi4j.io.gpio.Pin;
-import com.pi4j.io.gpio.PinMode;
 import com.pi4j.io.gpio.PinPullResistance;
 import com.pi4j.io.gpio.PinState;
 import com.pi4j.io.gpio.RaspiPin;
@@ -24,186 +23,186 @@ import com.pi4j.io.gpio.event.GpioPinDigitalStateChangeEvent;
 import com.pi4j.io.gpio.event.GpioPinListenerDigital;
 
 public class IODevice extends Device<IOConfig, GpioIOItemConfig> {
-	private static final Logger LOG = LoggerFactory.getLogger(IODevice.class);
-	
-	private Map<Byte, GpioPinDigitalOutput> pinOutputMap = new HashMap<Byte, GpioPinDigitalOutput>();
-	private Map<Byte, GpioPinDigitalInput> pinInputMap = new HashMap<Byte, GpioPinDigitalInput>();
-	
-	public IODevice() {
-		super();
-	}
-	
-	public IODevice(IOConfig config) {
-		super(config);
-	}
-	
-	public void startPolling(final GpioIOItemConfig itemConfig, final EventPublisher eventPublisher) {
-		GpioController controller = GpioFactory.getInstance();
-		GpioPinDigitalInput pin = null;
-		if (this.pinInputMap.containsKey(itemConfig.getPort())) {
-			pin = pinInputMap.get(itemConfig.getPort());
-		} else {
-			final Pin raspiPin = this.getRaspiPin(itemConfig.getPort());
-			LOG.debug("resolved to raspi-pin: " + raspiPin);
-			PinPullResistance pinPullResistance = PinPullResistance.PULL_DOWN;
-			if (itemConfig.isActiveLow()) {
-				pinPullResistance = PinPullResistance.PULL_UP;
-			}
-			pin = controller.provisionDigitalInputPin(raspiPin, pinPullResistance);
-			pinInputMap.put(itemConfig.getPort(), pin);
-		}
-		
-		if (pin.getListeners().size() > 0) {
-			return;
-		}
-		
-		LOG.debug("adding listener");
-		pin.addListener(new GpioPinListenerDigital() {
-			private long timeLastAction = 0;
-			
-			public void handleGpioPinDigitalStateChangeEvent(
-					GpioPinDigitalStateChangeEvent event) {
-				LOG.trace("pin {} change to {}", event.getPin().getPin().getAddress(), event.getState().isHigh());
-				
-				boolean on = event.getState().isHigh();
-				if (itemConfig.isActiveLow()) {
-					on = !on;
-				}
-				
-				if (on && (System.currentTimeMillis() - timeLastAction) < itemConfig.getDebounce()) {
-					LOG.trace("pin change is inside debounce interval, ignoring");
-					return;
-				}
-				
-				if (on) {
-					eventPublisher.sendCommand(itemConfig.getItemName(), OnOffType.ON);
-				} else {
-					eventPublisher.sendCommand(itemConfig.getItemName(), OnOffType.OFF);
-				}
-				
-				timeLastAction = System.currentTimeMillis();
-			}
-			
-		});
-	}
-	
-	public void stopPolling() {
-		for (GpioPinDigitalInput input : this.pinInputMap.values()) {
-			input.removeAllListeners();
-			input.setShutdownOptions(true, PinState.LOW, PinPullResistance.OFF);
-		}
-	}
+    private static final Logger LOG = LoggerFactory.getLogger(IODevice.class);
 
-	@Override
-	public State communicate(Command command, GpioIOItemConfig itemConfig, State state) {
-		LOG.debug("communicate with gpio device");
-		
-		GpioController controller = GpioFactory.getInstance();
-		LOG.debug("GPIO controller created");
-		
-		LOG.debug("setting output pin...");
-		GpioPinDigitalOutput pin = null;
-		if (this.pinOutputMap.containsKey(itemConfig.getPort())) {
-			pin = pinOutputMap.get(itemConfig.getPort());
-		} else {
-			final Pin raspiPin = this.getRaspiPin(itemConfig.getPort());
-			LOG.debug("resolved to raspi-pin: " + raspiPin);
-			pin = controller.provisionDigitalOutputPin(raspiPin);
-			pinOutputMap.put(itemConfig.getPort(), pin);
-		}
-		
-		
-		try {
-//			pin.export(PinMode.DIGITAL_OUTPUT);
-			if (command instanceof OnOffType) {
-				boolean on = false;
-				if (((OnOffType) command) == OnOffType.ON) {
-					on = true;
-				} else if (((OnOffType) command) == OnOffType.OFF) {
-					on = false;
-				} else {
-					throw new IllegalStateException("not implemented");
-				}
-				if (itemConfig.isActiveLow()) {
-					on = !on;
-				}
-				LOG.debug("set pin '" + itemConfig + "' to: " + on);
-				pin.setState(on);
-			} else {
-				throw new IllegalStateException("unsupported command: " + command);
-			}
-		} finally {
-//			pin.unexport();
-		}
-		return null;
-	}
-	
-	
-	private Pin getRaspiPin(Byte port) {
-		if (port == null) {
-			throw new IllegalStateException("port is not set");
-		}
-		LOG.debug("resolving port '" + port + "' to raspi pin");
-		switch (port) {
-		case 0:
-			return RaspiPin.GPIO_00;
-		case 1:
-			return RaspiPin.GPIO_01;
-		case 2:
-			return RaspiPin.GPIO_02;
-		case 3:
-			return RaspiPin.GPIO_03;
-		case 4:
-			return RaspiPin.GPIO_04;
-		case 5:
-			return RaspiPin.GPIO_05;
-		case 6:
-			return RaspiPin.GPIO_06;
-		case 7:
-			return RaspiPin.GPIO_07;
-		case 8:
-			return RaspiPin.GPIO_08;
-		case 9:
-			return RaspiPin.GPIO_09;
-		case 10:
-			return RaspiPin.GPIO_10;
-		case 11:
-			return RaspiPin.GPIO_11;
-		case 12:
-			return RaspiPin.GPIO_12;
-		case 13:
-			return RaspiPin.GPIO_13;
-		case 14:
-			return RaspiPin.GPIO_14;
-		case 15:
-			return RaspiPin.GPIO_15;
-		case 16:
-			return RaspiPin.GPIO_16;
-		case 17:
-			return RaspiPin.GPIO_17;
-		case 18:
-			return RaspiPin.GPIO_18;
-		case 19:
-			return RaspiPin.GPIO_19;
-		case 20:
-			return RaspiPin.GPIO_20;
-		default:
-			throw new IllegalStateException("not a valid port: " + port);
-		}
-	}
+    private Map<Byte, GpioPinDigitalOutput> pinOutputMap = new HashMap<Byte, GpioPinDigitalOutput>();
+    private Map<Byte, GpioPinDigitalInput> pinInputMap = new HashMap<Byte, GpioPinDigitalInput>();
 
-	@Override
-	public boolean equals(Object arg0) {
-		if (!(arg0 instanceof IODevice)) {
-			return false;
-		}
-		
-		return true;
-	}
+    public IODevice() {
+        super();
+    }
 
-	@Override
-	public int hashCode() {
-		return super.hashCode();
-	}
+    public IODevice(IOConfig config) {
+        super(config);
+    }
+
+    public void startPolling(final GpioIOItemConfig itemConfig, final EventPublisher eventPublisher) {
+        GpioController controller = GpioFactory.getInstance();
+        GpioPinDigitalInput pin = null;
+        if (this.pinInputMap.containsKey(itemConfig.getPort())) {
+            pin = pinInputMap.get(itemConfig.getPort());
+        } else {
+            final Pin raspiPin = this.getRaspiPin(itemConfig.getPort());
+            LOG.debug("resolved to raspi-pin: " + raspiPin);
+            PinPullResistance pinPullResistance = PinPullResistance.PULL_DOWN;
+            if (itemConfig.isActiveLow()) {
+                pinPullResistance = PinPullResistance.PULL_UP;
+            }
+            pin = controller.provisionDigitalInputPin(raspiPin, pinPullResistance);
+            pinInputMap.put(itemConfig.getPort(), pin);
+        }
+
+        if (pin.getListeners().size() > 0) {
+            return;
+        }
+
+        LOG.debug("adding listener");
+        pin.addListener(new GpioPinListenerDigital() {
+            private long timeLastAction = 0;
+
+            public void handleGpioPinDigitalStateChangeEvent(GpioPinDigitalStateChangeEvent event) {
+                LOG.trace("pin {} change to {}", event.getPin().getPin().getAddress(), event.getState().isHigh());
+
+                boolean on = event.getState().isHigh();
+                if (itemConfig.isActiveLow()) {
+                    on = !on;
+                }
+
+                if (on && (System.currentTimeMillis() - timeLastAction) < itemConfig.getDebounce()) {
+                    LOG.trace("pin change is inside debounce interval, ignoring");
+                    return;
+                }
+
+                if (on) {
+                    eventPublisher.sendCommand(itemConfig.getItemName(), OnOffType.ON);
+                } else {
+                    eventPublisher.sendCommand(itemConfig.getItemName(), OnOffType.OFF);
+                }
+
+                timeLastAction = System.currentTimeMillis();
+            }
+
+        });
+    }
+
+    public void stopPolling() {
+        for (GpioPinDigitalInput input : this.pinInputMap.values()) {
+            input.removeAllListeners();
+            input.setShutdownOptions(true, PinState.LOW, PinPullResistance.OFF);
+        }
+    }
+
+    @Override
+    public State communicate(Command command, GpioIOItemConfig itemConfig, State state) {
+        try {
+            LOG.debug("communicate with gpio device");
+            GpioController controller = GpioFactory.getInstance();
+            LOG.debug("GPIO controller created");
+            LOG.debug("setting output pin...");
+            GpioPinDigitalOutput pin = null;
+            if (this.pinOutputMap.containsKey(itemConfig.getPort())) {
+                pin = pinOutputMap.get(itemConfig.getPort());
+            } else {
+                final Pin raspiPin = this.getRaspiPin(itemConfig.getPort());
+                LOG.debug("resolved to raspi-pin: " + raspiPin);
+                pin = controller.provisionDigitalOutputPin(raspiPin);
+                pinOutputMap.put(itemConfig.getPort(), pin);
+                // pin.setMode(PinMode.DIGITAL_OUTPUT);
+                // pin.export(PinMode.DIGITAL_OUTPUT);
+            }
+            LOG.debug("pin mode for pin '{}': {}", pin.getName(), pin.getMode());
+            try {
+                if (command instanceof OnOffType) {
+                    boolean on = false;
+                    if (((OnOffType) command) == OnOffType.ON) {
+                        on = true;
+                    } else if (((OnOffType) command) == OnOffType.OFF) {
+                        on = false;
+                    } else {
+                        throw new IllegalStateException("not implemented");
+                    }
+                    if (itemConfig.isActiveLow()) {
+                        on = !on;
+                    }
+                    LOG.debug("set pin '" + itemConfig + "' to: " + on);
+                    pin.setState(on);
+                } else {
+                    throw new IllegalStateException("unsupported command: " + command);
+                }
+            } finally {
+                // pin.unexport();
+            }
+        } catch (Exception e) {
+            LOG.error("unknown error", e);
+        }
+        return null;
+    }
+
+    private Pin getRaspiPin(Byte port) {
+        if (port == null) {
+            throw new IllegalStateException("port is not set");
+        }
+        LOG.debug("resolving port '" + port + "' to raspi pin");
+        switch (port) {
+            case 0:
+                return RaspiPin.GPIO_00;
+            case 1:
+                return RaspiPin.GPIO_01;
+            case 2:
+                return RaspiPin.GPIO_02;
+            case 3:
+                return RaspiPin.GPIO_03;
+            case 4:
+                return RaspiPin.GPIO_04;
+            case 5:
+                return RaspiPin.GPIO_05;
+            case 6:
+                return RaspiPin.GPIO_06;
+            case 7:
+                return RaspiPin.GPIO_07;
+            case 8:
+                return RaspiPin.GPIO_08;
+            case 9:
+                return RaspiPin.GPIO_09;
+            case 10:
+                return RaspiPin.GPIO_10;
+            case 11:
+                return RaspiPin.GPIO_11;
+            case 12:
+                return RaspiPin.GPIO_12;
+            case 13:
+                return RaspiPin.GPIO_13;
+            case 14:
+                return RaspiPin.GPIO_14;
+            case 15:
+                return RaspiPin.GPIO_15;
+            case 16:
+                return RaspiPin.GPIO_16;
+            case 17:
+                return RaspiPin.GPIO_17;
+            case 18:
+                return RaspiPin.GPIO_18;
+            case 19:
+                return RaspiPin.GPIO_19;
+            case 20:
+                return RaspiPin.GPIO_20;
+            default:
+                throw new IllegalStateException("not a valid port: " + port);
+        }
+    }
+
+    @Override
+    public boolean equals(Object arg0) {
+        if (!(arg0 instanceof IODevice)) {
+            return false;
+        }
+
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        return super.hashCode();
+    }
 
 }
